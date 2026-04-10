7a7304a7ad39b2cb60b6633cff39f1eb07e82f60
Tracybot <Tracybot@local>
2026-04-10 13:21:50 +0200

    Merged tracy chain

--- vscode-extension/src/mockData.ts
+++ vscode-extension/src/mockData.ts
@@ -47,6 +47,29 @@ export function getMockData(): MockData {
             lines: [11]
           }
         ]
+      },
+      {
+        path: "example2.ts",
+        tasklets: [
+          {
+            model: "mock-model1",
+            name: "tasklet1",
+            prompt: "mock-prompt1 sdopfkasdfposadk",
+            lines: [2, 3]
+          },
+          {
+            model: "mock-model2",
+            name: "tasklet2",
+            prompt: "mock-prompt2 asdfasdpoifasodf",
+            lines: [4, 5]
+          },
+          {
+            model: "mock-model3",
+            name: "tasklet3",
+            prompt: "mock-prompt3 asdfasdpoifasodf",
+            lines: [8, 9, 11]
+          }
+        ]
       }
     ]
   };
